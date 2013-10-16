aa150f74358456ecc2cdb06f5b46e940110d07de
idsc3101 <idsc3101@gmail.com>
2013-10-16 16:07:52 -0500

    finished

--- ReverseString/ReverseString/Form1.vb
+++ ReverseString/ReverseString/Form1.vb
@@ -6,14 +6,14 @@ Public Class Form1
         ' Read the input from the user
         Dim st As String
         st = InputBox("Please type the initial string")
-
-        ' Compute the result
+        Dim i As Integer
+        Dim c As Char
         Dim result As String = ""
-
-
-
+        For i = (st.Length - 1) To 0 Step -1
+            c = CChar(st.Substring(i, 1))
+            result = result & CStr(c)
+        Next
         ' Report the output to the user
-        MessageBox.Show("Not implemented yet", "Result")
-
+        MessageBox.Show(result)
     End Sub
 End Class
